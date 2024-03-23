25c99731dde64d16c5632d5e5084c59ddfb12d47
Rohit Yadav <7rohit.y@gmail.com>
2024-03-23 22:13:06 +0530

    Update index.js(added vercels url)

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.use(cookieParser())
 app.use(express.json());
 app.use(
     cors({
-        origin:"http://localhost:3000",
+        origin:"study-notion-liard.vercel.app",
         credentials:true
     })
 );
